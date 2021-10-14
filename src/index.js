7f416c875ac52bc00e0ecb0240b29bb9abf4218f
Eddie Medina <eddiemedina110@gmail.com>
2021-10-14 12:09:30 -0600

    probando whatsapp

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 'use strict'
 require('dotenv').config()
 
+const { Server } = require("socket.io");
 const express = require('express')
 const port = (process.env.PORT || 4000)
 const cors = require('cors')
@@ -10,6 +11,24 @@ const Citas = require('./models/Citas')
 //inicializacion
 const app = express()
 const http = require('http').Server(app)
+const { Client } = require('whatsapp-web.js');
+const client = new Client();
+
+client.on('qr', (qr) => {    console.log('QR RECEIVED', qr);});
+
+client.on('ready', () => {    console.log('Client is ready!');});
+
+client.initialize();
+
+const io = new Server(http, { 
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+        credentials: true
+      }    
+});
+
+
 
 require('./config/database')
 
